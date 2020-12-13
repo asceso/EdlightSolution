37584a84485cbbd908b7cda3d20371c100c8f5a1
asceso <asceso111@gmail.com>
2020-12-13 16:54:24 +0300

    базовая структура

---
+++ DatabaseModels/UserModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DatabaseModels
+{
+    /// <summary>
+    /// Сущность пользователь
+    /// </summary>
+    public class UserModel
+    {
+        /// <summary>
+        /// ИД пользователя
+        /// </summary>
+        public Guid ID { get; set; }
+
+        /// <summary>
+        /// Логин пользователя
+        /// </summary>
+        public string Login { get; set; }
+
+        /// <summary>
+        /// Пароль пользователя
+        /// </summary>
+        public string Password { get; set; }
+    }
+}

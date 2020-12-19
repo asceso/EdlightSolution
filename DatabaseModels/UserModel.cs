ea3e41f69eb6546c407eca4071e257a6dbccddf9
asceso <asceso111@gmail.com>
2020-12-19 14:06:30 +0300

    база

--- DatabaseModels/UserModel.cs
+++ DatabaseModels/UserModel.cs
@@ -1,22 +1,27 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace DatabaseModels
 {
+    [DataContract]
     /// <summary>
     /// Сущность пользователь
     /// </summary>
     public class UserModel
     {
+        [DataMember]
         /// <summary>
         /// ИД пользователя
         /// </summary>
         public Guid ID { get; set; }
 
+        [DataMember]
         /// <summary>
         /// Логин пользователя
         /// </summary>
         public string Login { get; set; }
 
+        [DataMember]
         /// <summary>
         /// Пароль пользователя
         /// </summary>
---
+++ DatabaseServices/UsersService.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using DatabaseModels;
+using EdlightDataClient;
+
+namespace DatabaseServices
+{
+    public static class UsersService
+    {
+        public static async Task<UserModel> GetUserByLogin(string login)
+            => await EdlightProxy.Execute(client => client.GetUserByLogin(login));
+    }
+}
